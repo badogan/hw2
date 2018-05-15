851a98aa5d30d078913c5d28548a19334553d778
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 21:10:07 +0100

    added a function

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -1,9 +1,15 @@
-var num1 = parseInt(window.prompt("Enter a number"));
+function getuserinput()
+{
+    var number_entered = parseInt(window.prompt("Enter a number"));
+    return number_entered
+}
 
+num1 = getuserinput()
 /*var num1 = 804;*/
 
 var numbersString = "number entered is ";
 var myNum = 2;
+/*var myArray = ["a", "b", "c", "d", "e"]; */
 
 if(num1 % 2 == 0)
 {
@@ -12,6 +18,8 @@ while(myNum > 0)
 {
     document.getElementById("decision_box").innerHTML += numbersString + "even";
     myNum = myNum - 1;
+    /*for(x = 0; x < 4; x++) IN CASE I NEED IT LATER*/
+    /* myArray[3];*/ 
 }
 }
 else{
