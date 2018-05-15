265f1868bd7ec894bfc83c67d922e8eff3f42107
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 21:00:05 +0100

    adding while (a bit funny though)

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -3,11 +3,16 @@ var num1 = parseInt(window.prompt("Enter a number"));
 /*var num1 = 804;*/
 
 var numbersString = "number entered is ";
+var myNum = 2;
 
 if(num1 % 2 == 0)
 {
   /*document.write("num1 is even");*/
-  document.getElementById("decision_box").innerHTML += numbersString + "even";
+while(myNum > 0)
+{
+    document.getElementById("decision_box").innerHTML += numbersString + "even";
+    myNum = myNum - 1;
+}
 }
 else{
   /*document.write("num1 is odd");*/
