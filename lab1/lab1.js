bfbf9e74bc37ebb3997717182a2bdc27289ccbc0
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 10:26:37 +0100

    continues

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -3,4 +3,10 @@ var userName = window.prompt("Please enter your name");
 var greetingParagraph = document.getElementById("greeting");
 greetingParagraph.innerHTML = greetingParagraph.innerHTML+"," + userName;
 
+var num1 = parseInt(window.prompt("Enter a number"));
+var num2 = parseInt(window.prompt("Enter another number"));
+var operand1 = document.getElementById("operand1");
+var operand2 = document.getElementById("operand2");
+operand1.innerHTML += num1;
+operand2.innerHTML += num2;
 
