f22f153cbd76b21f0d1d7da2103ec7f3e4dc515c
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 09:51:47 +0100

    some variables

--- helloScript.js
+++ helloScript.js
@@ -2,3 +2,11 @@ var msg = "Hello, world!";
 alert(msg);
 console.log("Hello, console!");
 document.write("Hello, document!");
+
+var contact = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+};
+contact.age++;  //increments age by 1
+alert(contact.age); //displays the value 26
