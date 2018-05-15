1abfc4bd8c5eba1056b4a666a9eec190e1b6ba50
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 10:34:13 +0100

    adding messages

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -10,11 +10,15 @@ var operand2 = document.getElementById("operand2");
 operand1.innerHTML += num1;
 operand2.innerHTML += num2;
 
-var sum = num1 + num2;
-var multiply = num1 * num2;
-var divide = num1 / num2;
-var subtract = num1 - num2;
-var modulus = num1 % num2;
+var result_addition = num1 + num2;
+var result_multiplication = num1 * num2;
+var result_division = num1 / num2;
+var result_subtraction = num1 - num2;
+var result_modulus = num1 % num2;
 
-
+document.getElementById("addition").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + result_addition;
+document.getElementById("subtraction").innerHTML = "The subtraction of " + num1 + " and " + num2 + " is " + result_subtraction;
+document.getElementById("multiplication").innerHTML = "The multiplication of " + num1 + " and " + num2 + " is " + result_multiplication;
+document.getElementById("division").innerHTML = "The division of " + num1 + " and " + num2 + " is " + result_division;
+document.getElementById("modulus").innerHTML = "The modulus of " + num1 + " and " + num2 + " is " + result_modulus;
 
