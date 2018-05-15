69ecd90767a6a393f7fdb844d71bd5d4ff00b26b
badogan <38670005+badogan@users.noreply.github.com>
2018-05-15 20:50:33 +0100

    doing it right :)

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -2,12 +2,14 @@ var num1 = parseInt(window.prompt("Enter a number"));
 
 /*var num1 = 804;*/
 
+var numbersString = "number entered is ";
+
 if(num1 % 2 == 0)
 {
   /*document.write("num1 is even");*/
-  document.getElementById("decision_box").innerHTML = "number entered is even";
+  document.getElementById("decision_box").innerHTML += numbersString + "even";
 }
 else{
   /*document.write("num1 is odd");*/
-  document.getElementById("decision_box").innerHTML = "number entered is odd";
+  document.getElementById("decision_box").innerHTML += numbersString + "odd";
 }
