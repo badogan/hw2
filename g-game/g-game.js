6384f8cde41f8c0cca6e1abe3009b812d9b63529
badogan <38670005+badogan@users.noreply.github.com>
2018-05-16 17:28:42 +0100

    wip

--- g-game/g-game.js
+++ g-game/g-game.js
@@ -1,15 +1,19 @@
 // program data:
 var number = 4;
 var guess;
+var limit=5;
 
-// prompt user for their guess 
-guess = prompt("Guess a number");
+for (i = 1; i <= limit; i++){
+    // prompt user for their guess 
+    guess = prompt("Guess a number");
 
-// if correct: let the user know they won
-if(guess == number){
-    document.write("Correct! You won.")
-}
-// if incorrect: let the user know
-else{
-    alert("incorrect");
+    // if correct: let the user know they won
+    if(guess == number){
+        document.write("Correct! You won.");
+        break;
+    }
+    // if incorrect: let the user know how  many tries they have remaining
+    else{
+        alert("incorrect. \nTries remaining: " + (limit-1))
+    }
 }
