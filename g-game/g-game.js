3b1f7d9e47128145b9822cb19bcece79deaf6047
badogan <38670005+badogan@users.noreply.github.com>
2018-05-16 17:40:05 +0100

    wip

--- g-game/g-game.js
+++ g-game/g-game.js
@@ -8,7 +8,8 @@ var guesses=[]
 
 for (i = 1; i <= limit; i++){
     // prompt user for their guess 
-    guess = prompt("Guess a number");
+   // this is how we started: guess = prompt("Guess a number");
+    guess = parseInt(prompt("Guess a number"));
 
     // if correct: let the user know they won
     if(guess == number){
@@ -25,4 +26,13 @@ for (i = 1; i <= limit; i++){
 
 if (!won){
     document.write("Sorry, you ran out of tries! Game over!"); 
+}
+
+function isPreviousGuess(){
+    for(i2 = 0; i2 < guesses.length; i2++){
+        if(guesses[i2] = guess){
+            return true;
+        }
+    }
+    return false;
 }
