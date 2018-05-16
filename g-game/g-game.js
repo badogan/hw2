2cd87295155d7fcee9dd6bd6fe51a0671a3428c8
badogan <38670005+badogan@users.noreply.github.com>
2018-05-16 17:22:00 +0100

    initial

---
+++ g-game/g-game.js
@@ -0,0 +1,15 @@
+// program data:
+var number = 4;
+var guess;
+
+// prompt user for their guess 
+guess = prompt("Guess a number");
+
+// if correct: let the user know they won
+if(guess == number){
+    document.write("Correct! You won.")
+}
+// if incorrect: let the user know
+else{
+    alert("incorrect");
+}
