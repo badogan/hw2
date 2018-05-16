9e4484d76615418ab4db964a3cc0c412bcf27c7e
badogan <38670005+badogan@users.noreply.github.com>
2018-05-16 17:32:44 +0100

    wip

--- g-game/g-game.js
+++ g-game/g-game.js
@@ -2,6 +2,7 @@
 var number = 4;
 var guess;
 var limit=5;
+var won=false;
 
 for (i = 1; i <= limit; i++){
     // prompt user for their guess 
@@ -10,10 +11,15 @@ for (i = 1; i <= limit; i++){
     // if correct: let the user know they won
     if(guess == number){
         document.write("Correct! You won.");
+        won=true
         break;
     }
     // if incorrect: let the user know how  many tries they have remaining
     else{
         alert("incorrect. \nTries remaining: " + (limit-i))
     }
+}
+
+if (!won){
+    document.write("Sorry, you ran out of tries! Game over!"); 
 }
