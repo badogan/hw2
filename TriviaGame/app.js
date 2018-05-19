eb8831154de5fa7405f61444c363cfdad2f9d083
badogan <38670005+badogan@users.noreply.github.com>
2018-05-19 18:49:10 +0100

    wip

--- TriviaGame/app.js
+++ TriviaGame/app.js
@@ -21,8 +21,8 @@ function populateQuestions(){
     // populate with default questions
     questions = defaultQuestions;
     // if local storage contains questions, add to question set
-    if(localStorage.getItem("questions")){
-        var storedQuestions = JSON.parse(localStorage.getItem("questions"));
+    if(localStorage.getItem("questions2")){
+        var storedQuestions = JSON.parse(localStorage.getItem("questions2"));
         for(var i = 0; i < storedQuestions.length; i++){
             questions.push(storedQuestions[i]);
         }
