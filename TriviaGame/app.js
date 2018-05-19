a1aaa049dfe4e68997f9916eb20511443ce272b6
badogan <38670005+badogan@users.noreply.github.com>
2018-05-19 19:03:57 +0100

    wip

--- TriviaGame/app.js
+++ TriviaGame/app.js
@@ -1,9 +1,10 @@
 // initialize variables
-var questionID, question, choiceA, choiceB, choiceC, choiceD, questions, numQuestions, qInfo,
-current = 0;
+var questionID, question, choiceA, choiceB, choiceC, choiceD, questions, numQuestions, qInfo, userChoice
+current = 0,
+score = 0,
+points = [];
 
 // reference HTML elements
-var elQuiz = document.getElementById("quiz");
 var elQuizStatus = document.getElementById("quizStatus");
 
 var elQuestion = document.getElementById("question");
@@ -16,6 +17,7 @@ var elChoices = document.getElementsByName('choices');
 // start quiz
 populateQuestions();
 renderQuestion();
+document.getElementById("submit").onclick = gradeQuestion;
 
 function populateQuestions(){
     // populate with default questions
@@ -51,4 +53,37 @@ function renderQuestion(){
     elChoiceB.innerHTML = choiceB;
     elChoiceC.innerHTML = choiceC;
     elChoiceD.innerHTML = choiceD;
+}
+
+function gradeQuestion(){
+    if(getUserChoice()){
+        if(userChoice == questions[current].correct){
+            score++;
+            points[current] = 1;
+        }
+        else{
+            points[current] = 0;
+        }
+        // next question
+        current++;
+        renderQuestion();
+    }
+}
+
+function getUserChoice(){
+    for (var i = 0, length = elChoices.length; i < length; i++)
+    {
+        if (elChoices[i].checked)
+        {
+            userChoice = elChoices[i].value;
+
+            // clear radio input for next question
+            elChoices[i].checked = false;
+
+            return true;
+        }
+    }
+    // user didn't select an answer
+    alert("Please select an answer before continuing");
+    return false;
 }
