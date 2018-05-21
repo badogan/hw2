12c3a285929eb034908a7ee2a6726c5bae7a28dc
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 20:51:21 +0100

    wip

--- nodejs-to-AzureSQL/sqltest.js
+++ nodejs-to-AzureSQL/sqltest.js
@@ -1,5 +1,6 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;  
 
 // Create connection to database
 var config = 
