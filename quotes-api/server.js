fde4a661e7ac8eee9aa4ff51a7f0ea5f189ddbb4
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 09:15:42 +0100

    initial

---
+++ quotes-api/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = 3001;
+app.listen(port, function(){
+    console.log('Basri-Express app listening on port ' + port);
+});
+
+// I added "get " to path and it became /get. I think it is important to be clear 
+// (for me in this stage at least) :)
+
+app.get('/get', function(request, response){
+    response.send('3001 test Basri says Hello, World!');
+});
