4dcc54536e1bfa5bdfd60fc74a9e3fc2a616037b
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 09:23:27 +0100

    wip

--- quotes-api/server.js
+++ quotes-api/server.js
@@ -8,6 +8,34 @@ app.listen(port, function(){
 // I added "get " to path and it became /get. I think it is important to be clear 
 // (for me in this stage at least) :)
 
-app.get('/get', function(request, response){
+app.get('/', function(request, response){
     response.send('Basri get request received');
 });
+
+var quotes = [
+    {
+        id: 1,
+        quote: "The best is yet to come",
+        author: "Unknown",
+        year: 2000
+    },
+    {
+        id: 2,
+        quote: "This is a quote",
+        author: "First Last",
+        year: 1930
+    },
+    {
+        id: 3,
+        quote: "This is another quote",
+        author: "First2 Last2",
+        year: 1910
+    }
+    ];
+
+
+app.get('/quotes', function(req, res){
+    console.log("Get a list of all quotes as json");
+    res.json(quotes);
+});
+
