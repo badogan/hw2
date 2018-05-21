fd11b6d0c72162ba6369497d1e5f3f867fbb47f6
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 20:37:09 +0100

    wip

--- nodejs-to-AzureSQL/sqltest.js
+++ nodejs-to-AzureSQL/sqltest.js
@@ -35,7 +35,8 @@ function queryDatabase()
 
        // Read all rows from table
      request = new Request(
-          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+          //"SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+          "SELECT * from contacts",
              function(err, rowCount, rows) 
                 {
                     console.log(rowCount + ' row(s) returned');
