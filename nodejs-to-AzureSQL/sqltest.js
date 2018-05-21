94e63d01307c2cbaf90ec0949c2e0947648f9711
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 20:36:16 +0100

    wip

--- nodejs-to-AzureSQL/sqltest.js
+++ nodejs-to-AzureSQL/sqltest.js
@@ -1,44 +1,52 @@
-var Connection = require('tedious').Connection;  
-    var config = {  
-        userName: 'badogan',  
-        password: 'To$hiba22123',  
-        server: 'basri-db.database.windows.net',  
-        // When you connect to Azure SQL Database, you need these next options.  
-        options: {encrypt: true, database: 'exercise-db'}  
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.  
-        console.log("Connected");  
-        executeStatement();  
-    });  
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-    var Request = require('tedious').Request;  
-    var TYPES = require('tedious').TYPES;  
+// Create connection to database
+var config = 
+   {
+    userName: 'badogan',  
+    password: 'To$hiba22123',  
+    server: 'basri-db.database.windows.net',  
 
-    function executeStatement() {  
-        //request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {  
-        request = new Request("select * from contacts", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            console.log("I am here!")
-            result ="";  
-        });  
+     options: 
+        {
+           database: 'exercise-db' //update me
+           , encrypt: true
+        }
+   }
+var connection = new Connection(config);
 
-        //request.on('Done',function(rowCount,more) {  
-        //console.log(rowCount + ' rows returned');  
-        //});  
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err)
+       }
+    else
+       {
+           queryDatabase()
+       }
+   }
+ );
 
-        connection.execSql(request);  
-    } 
+function queryDatabase()
+   { console.log('Reading rows from the Table...');
+
+       // Read all rows from table
+     request = new Request(
+          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+             function(err, rowCount, rows) 
+                {
+                    console.log(rowCount + ' row(s) returned');
+                    process.exit();
+                }
+            );
+
+     request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+         });
+             });
+     connection.execSql(request);
+   }
