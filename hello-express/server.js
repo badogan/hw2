d893d52aa2c64ce8a2c41b3493298e2c7e1cd866
badogan <38670005+badogan@users.noreply.github.com>
2018-05-21 09:06:56 +0100

    wip

--- hello-express/server.js
+++ hello-express/server.js
@@ -5,6 +5,9 @@ app.listen(port, function(){
     console.log('Basri-Express app listening on port ' + port);
 });
 
+// I added "get " to path and it became /get. I think it is important to be clear 
+// (for me in this stage at least) :)
+
 app.get('/get', function(request, response){
     response.send('Basri says Hello, World!');
 });
